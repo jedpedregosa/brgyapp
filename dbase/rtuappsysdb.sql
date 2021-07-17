926f7481876c4d5b3364c3956f1eb0af43355c02
Edward <wardpedregosa@gmail.com>
2021-07-17 17:51:28 +0800

    dbase update

--- dbase/rtuappsysdb.sql
+++ dbase/rtuappsysdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 11, 2021 at 07:03 PM
+-- Generation Time: Jul 17, 2021 at 09:47 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.4.9
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `tbl_appointment` (
   `app_done_date` date DEFAULT NULL,
   PRIMARY KEY (`app_id`),
   UNIQUE KEY `app_num` (`app_num`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `tbl_employee_data` (
   `employee_num` varchar(25) NOT NULL,
   PRIMARY KEY (`vstor_id`),
   UNIQUE KEY `employee_num` (`employee_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `tbl_guest_data` (
   `company` varchar(125) NOT NULL,
   `government_id` varchar(125) NOT NULL,
   PRIMARY KEY (`vstor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -82,19 +82,26 @@ CREATE TABLE IF NOT EXISTS `tbl_office` (
   `office_id` varchar(25) NOT NULL,
   `office_name` varchar(250) NOT NULL,
   `office_desc` varchar(500) NOT NULL,
+  `office_branch` varchar(50) NOT NULL,
   PRIMARY KEY (`office_id`),
   KEY `office_num` (`office_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_office`
 --
 
-INSERT INTO `tbl_office` (`office_num`, `office_id`, `office_name`, `office_desc`) VALUES
-(1, 'RTU-O01', 'Curriculum and Instructional Resources Development Center', ''),
-(2, 'RTU-O02', 'Alumni Relations and Placement Office', ''),
-(3, 'RTU-O03', 'Disaster Risk Protection Office', ''),
-(4, 'RTU-O04', 'University Data Protection Office', '');
+INSERT INTO `tbl_office` (`office_num`, `office_id`, `office_name`, `office_desc`, `office_branch`) VALUES
+(1, 'RTU-O01', 'Curriculum and Instructional Resources Development Center', '', 'Pasig Campus'),
+(2, 'RTU-O02', 'Alumni Relations and Placement Office', '', 'Pasig Campus'),
+(3, 'RTU-O03', 'Disaster Risk Protection Office', '', 'Pasig Campus'),
+(4, 'RTU-O04', 'University Data Protection Office', '', 'Pasig Campus'),
+(5, 'RTU-O05', 'Student Records and Admission Center', '', 'Boni Campus'),
+(6, 'RTU-O06', 'Scholarship and Grant Office', '', 'Boni Campus'),
+(7, 'RTU-O07', 'Graduate School', '', 'Boni Campus'),
+(8, 'RTU-O08', 'Human Resource Development Center', '', 'Boni Campus'),
+(9, 'RTU-O09', 'College of Engineering, Architecture and Technology', '', 'Boni Campus'),
+(10, 'RTU-O10', 'Guidance Services Center', '', 'Boni Campus');
 
 -- --------------------------------------------------------
 
@@ -113,7 +120,25 @@ CREATE TABLE IF NOT EXISTS `tbl_schedule` (
   `sched_is_available` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`sched_id`),
   KEY `sched_num` (`sched_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_schedule`
+--
+
+INSERT INTO `tbl_schedule` (`sched_num`, `sched_id`, `tmslot_id`, `office_id`, `sched_date`, `sched_total_visitor`, `sched_is_available`) VALUES
+(261, '2107190101', 'TMSLOT-01', 'RTU-O01', '2021-07-19', 5, 0),
+(262, '2107190102', 'TMSLOT-02', 'RTU-O01', '2021-07-19', 2, 1),
+(263, '2107191001', 'TMSLOT-01', 'RTU-O10', '2021-07-19', 1, 1),
+(225, '2108110109', 'TMSLOT-09', 'RTU-O01', '2021-08-11', 5, 0),
+(225, '2108110110', 'TMSLOT-010', 'RTU-O01', '2021-08-11', 5, 0),
+(225, '2108110111', 'TMSLOT-011', 'RTU-O01', '2021-08-11', 5, 0),
+(225, '2108110112', 'TMSLOT-012', 'RTU-O01', '2021-08-11', 5, 0),
+(227, '2108110113', 'TMSLOT-013', 'RTU-O01', '2021-08-11', 5, 0),
+(227, '2108110114', 'TMSLOT-014', 'RTU-O01', '2021-08-11', 5, 0),
+(227, '2108110115', 'TMSLOT-015', 'RTU-O01', '2021-08-11', 5, 0),
+(228, '2108190114', 'TMSLOT-14', 'RTU-O01', '2021-08-19', 1, 1),
+(13, '2108190304', 'TMSLOT-04', 'RTU-O03', '2021-08-19', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -127,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `tbl_student_data` (
   `student_num` varchar(25) NOT NULL,
   PRIMARY KEY (`vstor_id`),
   UNIQUE KEY `student_num` (`student_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -142,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `tbl_timeslot` (
   `tmslot_start` varchar(25) NOT NULL,
   `tmslot_end` varchar(25) NOT NULL,
   PRIMARY KEY (`tmslot_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_timeslot`
@@ -184,7 +209,7 @@ CREATE TABLE IF NOT EXISTS `tbl_visitor` (
   PRIMARY KEY (`vstor_num`) USING BTREE,
   UNIQUE KEY `vstor_id` (`vstor_id`),
   UNIQUE KEY `vstor_email` (`vstor_email`)
-) ENGINE=MyISAM AUTO_INCREMENT=206 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=247 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
