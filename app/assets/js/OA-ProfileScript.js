2d70b37bbedf55135f5bb5074bdd42a1ca3b96f7
KL00 <41349849+KyleMiguelLaurente@users.noreply.github.com>
2021-08-19 14:23:49 +0800

    profile validation
    pw checker not finalized

--- app/assets/js/OA-ProfileScript.js
+++ app/assets/js/OA-ProfileScript.js
@@ -295,3 +295,89 @@ function valid() {
     conPass.style.borderBottom = "2px solid green";
   }
 }
+
+
+//input fields
+const first_name = document.getElementById('first-name');
+const last_name = document.getElementById('last-name');
+const email_address = document.getElementById('email-address');
+const contact_number = document.getElementById('contact-number');
+
+//err0r-labels
+const error_first_name = document.getElementById('error-first-name');
+const error_last_name = document.getElementById('error-last-name');
+const error_email_address = document.getElementById('error-email-address');
+const error_contact_number = document.getElementById('error-contact-number');
+
+var wordRegex = /^[a-zA-Z ]*$/;
+var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var numberRegex = /^[0-9-+]*$/;
+
+function isFormValidated() {
+    return isFirstNameValid() && isLastNameValid() && isEmailValid() && isContactValid();
+}
+function isFirstNameValid() {
+    if(first_name.value == null || first_name.value == ''){
+        error_first_name.textContent = "Please fill up this field";
+    } else if (!first_name.value.match(wordRegex) || first_name.value.length < 2){
+        error_first_name.textContent = "Invalid First Name";
+    } else{
+        error_first_name.textContent ="";
+        return true;
+    }
+    return false;
+}
+
+function isLastNameValid() {
+    if(last_name.value == null || last_name.value == ''){
+        error_last_name.textContent = "Please fill up this field";
+    } else if (!last_name.value.match(wordRegex) || last_name.value.length < 2){
+        error_last_name.textContent = "Invalid Last Name";
+    } else{
+        error_last_name.textContent="";
+        return true;
+    }
+    return false;
+}
+
+function isEmailValid() {
+    if(email_address.value == null || email_address.value == ''){
+        error_email_address.textContent = "Please fill up this field";
+    } else if (!email_address.value.match(emailRegex) || email_address.value.length < 2){
+        error_email_address.textContent = "Invalid Email Address";
+    } else {
+        if(error_email_address) {
+            error_email_address.textContent="";
+        }
+        return true;
+    }
+    return false;
+}
+
+function isContactValid() {
+    if(contact_number.value == null || contact_number.value == ''){
+        error_contact_number.textContent = "Please fill up this field";
+    } else if (!contact_number.value.match(numberRegex) || contact_number.value.length > 15 || contact_number.value.length < 6){
+        error_contact_number.textContent = "Invalid Contact Number";
+    } else{
+        error_contact_number.textContent="";
+        return true;
+    }
+    return false;
+}
+
+first_name.addEventListener("keyup", function(event){
+    isFirstNameValid();
+});
+
+last_name.addEventListener("keyup", function(event){
+    isLastNameValid();
+});
+
+email_address.addEventListener("keyup", function(event){
+    isEmailValid();
+});
+
+contact_number.addEventListener("keyup", function(event){
+    isContactValid();
+});
